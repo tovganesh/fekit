f24352a2dd253af5fad8c3705c467e009221ffdb
V. Ganesh <tovganesh@acm.org>
2024-08-20 23:34:11 +0530

    template atom properties (#18)

---
+++ fekit/src/atomprop/mod.rs
@@ -0,0 +1,15 @@
+/**
+ * Author: V. Ganesh
+ * License: MIT
+ */
+
+/** defines commonly used atomic properties such as atomic number, weight and covalent radii */
+
+/** atomic number */
+const ATOMIC_NUMBER: [1, 2, 3, 4, 5, 6];
+
+/** atomic weight */
+const ATOMIC_WEIGHT: [1.00784, 4.002602, 6.938, 9.0121831, 10.806, 12.0096];
+
+/** covalent radii */
+const COVALENT_RADII: [32, 46, 133, 102, 85, 75];
